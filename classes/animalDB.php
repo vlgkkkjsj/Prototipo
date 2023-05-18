5fee3695b4d6ff92496f529ef3559d10bf12853d
Victor <vic.vl674@gmail.com>
2023-05-18 14:15:43 -0300

    primeiro commit

---
+++ classes/animalDB.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class AnimalDB
+{
+    private $conexao;
+
+    public function __construct()
+    {
+        $this->conexao = new conexao();
+    }
+    public function cadastrarAnimal($animal,$localizacao,$email)
+    {
+       if(empty($animal) or empty($localizacao) or empty($email))
+       {
+            print "<script>alert('ceritifique-se que informou todas as informacoes corretamente')</script>";
+            print "<script>location:formParaAdotar.php</script>";
+       }
+       else
+       {
+        $result = "INSERT INTO paraadotar (animal,localizacao,email) VALUES ('{$animal}','{$localizacao}','{$email}')";
+
+        $res = mysqli_query($this->conexao->getConn(),$result);
+
+        if(mysqli_affected_rows($this-> conexao ->getConn())>0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+       }
+    }
+}
